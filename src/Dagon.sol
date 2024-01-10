0658b84e74dde08616ed63a3154de9b79d66d3c1
ross <92001561+z0r0z@users.noreply.github.com>
2024-01-10 08:52:58 +0000

    🧹 Tidy ~~

--- src/Dagon.sol
+++ src/Dagon.sol
@@ -346,10 +346,10 @@ contract Dagon is ERC6909 {
         assembly ("memory-safe") {
             mstore(0x00, 0x70a08231000000000000000000000000) // `balanceOf(address)`.
             mstore(0x14, account) // Store the `account` argument.
-            if iszero(staticcall(gas(), token, 0x10, 0x24, 0x00, 0x20)) {
+            if iszero(staticcall(gas(), token, 0x10, 0x24, 0x20, 0x20)) {
                 revert(codesize(), codesize()) // For gas estimation.
             }
-            amount := mload(0x00)
+            amount := mload(0x20)
         }
     }
 
@@ -376,10 +376,10 @@ contract Dagon is ERC6909 {
     function _totalSupply(address token) internal view virtual returns (uint256 supply) {
         assembly ("memory-safe") {
             mstore(0x00, 0x18160ddd) // `totalSupply()`.
-            if iszero(staticcall(gas(), token, 0x1c, 0x04, 0x00, 0x20)) {
+            if iszero(staticcall(gas(), token, 0x1c, 0x04, 0x20, 0x20)) {
                 revert(codesize(), codesize()) // For gas estimation.
             }
-            supply := mload(0x00)
+            supply := mload(0x20)
         }
     }
 
@@ -393,10 +393,10 @@ contract Dagon is ERC6909 {
         assembly ("memory-safe") {
             mstore(0x00, 0xbd85b039) // `totalSupply(uint256)`.
             mstore(0x20, id) // Store the `id` argument.
-            if iszero(staticcall(gas(), token, 0x1c, 0x24, 0x00, 0x20)) {
+            if iszero(staticcall(gas(), token, 0x1c, 0x24, 0x20, 0x20)) {
                 revert(codesize(), codesize()) // For gas estimation.
             }
-            supply := mload(0x00)
+            supply := mload(0x20)
         }
     }
 
